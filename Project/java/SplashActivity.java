04ff0e1f7d770e53cb60c73c5776c78d0821d7d4
MasterKalajengking <147806658+MasterKalajengking@users.noreply.github.com>
2025-05-24 13:37:02 +0700

    Add files via upload

---
+++ Project/java/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.tst;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView logo = findViewById(R.id.logo);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(fadeIn);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 2000);
+    }
+}
